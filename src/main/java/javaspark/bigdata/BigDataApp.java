5456183078dc16f3a91e45bcef2f8e9dd16407e8
bigdatasys <dr.maciej.majewski@gmail.com>
2020-08-11 22:41:57 +0200

    update

--- src/main/java/javaspark/bigdata/BigDataApp.java
+++ src/main/java/javaspark/bigdata/BigDataApp.java
@@ -1,7 +1,15 @@
 package javaspark.bigdata;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 public class BigDataApp {
     public static void main(String[] args) {
         System.out.println("BigData App...");
+
     }
 }
